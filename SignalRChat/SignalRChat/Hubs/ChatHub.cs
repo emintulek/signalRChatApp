8f91ac290339397a5c7504b80211de98776d2bd9
Furkan UZUNÇAKMAK <furkan.uzuncakmak@ogr.sakarya.edu.tr>
2018-06-28 19:50:28 +0300

    3. Adım (Son)
    Broadcast, private ve grup mesaj özelliği eklendi.

---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRChat
+{
+    public class ChatHub:Hub    // chat işlemlerimizi bu class yapacak.
+    {
+        public void sendbroadcast(string name, string message)   // broadcast mesaj gönderir.
+        {
+            Clients.All.broadcastMessage(name + " | broadcast msg.", message);
+        }
+        public void sendgroup(string name, string message, string groupName)  // grup mesajı gönderir.
+        {
+            Clients.Group(groupName).broadcastMessage(name + " | group msg.", message);
+        }
+
+        public void sendtouser(string name, string message, string receiverConId, string selfConId)  // özel mesaj gönderir.
+        {
+            Clients.Client(receiverConId).broadcastMessage(name + " | private msg.", message); 
+            Clients.Client(selfConId).broadcastMessage(name + " | private msg.", message);
+        }
+        public void join(string roomName)   // kullanıcıyı gruba ekler.
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+        }
+      
+    }
+}
---
+++ SignalRChat/SignalRChat/scripts/chat.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    var chat = $.connection.chatHub;
+    chat.client.broadcastMessage = function (name, message) {
+        var mesaj = "<li><div class='message-data'>" +
+            "<span class='message-data-name'>" + name + "</span>" +
+            "</div>" +
+            "<div class='message my-message'>" + message + "</div></li>";
+        $('#discussion').append(mesaj);
+    };
+
+    $('#userName').text(prompt('Enter your name:', ''));
+    $('#groupName').text(prompt('Enter your group name:', ''));
+
+    $('#message').focus();
+    // server bağlantısı başlatılıyor.
+    $.connection.hub.start().done(function () {
+        console.log("Server'a bağlanıldı.");
+        jQuery('#message').keydown(function (event) {
+            if (event.keyCode == 13) {  // textarea da enter'a basınca mesajı gönderir.
+                if ($('#kime').val() != "") {   // özel mesaj atılıyor.
+                    chat.server.sendtouser($('#userName').text(), $('#message').val(), $('#kime').val(), $('#connID').text());
+                }
+                if ($('#grupMesaj').is(":checked")) {     //grup mesajı atılılıyor.                        
+                    chat.server.sendgroup($('#userName').text(), $('#message').val(), $('#groupName').text());                    
+                }
+                if ($('#kime').val() == "" && !$('#grupMesaj').is(':checked')) {   // broadcast mesaj atılıyor.
+                    chat.server.sendbroadcast($('#userName').text(), $('#message').val());                   
+                }
+                $('#message').val('').focus();
+                return false;
+            }
+        });
+
+        $(document).ready(function () {
+            $('#connID').text($.connection.hub.id);            
+            chat.server.join($('#groupName').text());            
+        });
+        $('#grupMesaj').click(function () {
+            if ($(this).is(':checked')) {
+                document.getElementById("kime").disabled = true;
+                $("#kime").val("");
+            }
+            else {
+                document.getElementById("kime").disabled = false;
+            }
+        });
+    });
+});
